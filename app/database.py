f0f830d9ade9b69ff8d566566833b03384400a99
adrian andi <adriana.fardan69@gmail.com>
2026-05-19 21:39:29 +0700

    database config

--- app/database.py
+++ app/database.py
@@ -3,10 +3,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "mysql+pymysql://root:@localhost/giziku"
-)
+# DATABASE_URL = os.getenv(
+#     "DATABASE_URL",
+#     "mysql+pymysql://root:@localhost/giziku"
+# )
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
